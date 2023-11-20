5f7963b25b3c9ead2bdd246b2b7d7e8483e43e6c
CatSci <atul.yadav@catsci.com>
2023-11-20 11:30:34 +0000

    bms data tool

---
+++ bms.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pandas as pd
+
+from utils import read_file, fill_data, remove_shipping, convert_duplicate_order_to_dataframe
+
+# hide streamlit style
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+# background-color: #ed9439;
+st.markdown("""
+<style>
+.navbar {
+  height: 80px;
+  background-color: #ed9439;
+  color: #ed9439;
+}
+.navbar-brand{
+    font-size: 40px;
+    margin-left:40px;
+}
+</style>""", unsafe_allow_html= True)
+
+
+st.markdown("""
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark">
+  <a class="navbar-brand" href="#" target="_blank">CatSci</a>
+  
+
+</nav>
+""", unsafe_allow_html=True)
+
+st.title('BMS Order Data Aggregation')
+
+uploaded_files = st.file_uploader("Choose a file", accept_multiple_files= True)
+
+
+if st.button("Done") and uploaded_files is not None:
+    with st.spinner('Please wait...'):
+        order_df, bms_df = read_file(uploaded_files)
+        # st.data_editor(bms_df)
+
+        bms_dataframe, duplicate_po_number = fill_data(bms_dataframe= bms_df, order_dataframe= order_df)
+        bms_dataframe, rows_to_remove = remove_shipping(bms_dataframe = bms_dataframe)
+        
+        duplicate_order_number = convert_duplicate_order_to_dataframe(duplicate_order_numbers= duplicate_po_number)
+        final_df = bms_dataframe.drop(rows_to_remove)
+        st.dataframe(final_df)
+        st.dataframe(duplicate_order_number)
+    
+
+
---
+++ utils.py
@@ -0,0 +1,127 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import Levenshtein
+import streamlit as st
+
+def read_file(uploaded_files):
+    """_summary_
+
+    Args:
+        uploaded_files (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    for uploaded_file in uploaded_files:
+        if "Order" in uploaded_file.name and "xlsx" in uploaded_file.name:
+            order_df = pd.read_excel(uploaded_file)
+            # st.write(uploaded_file.name)
+            # st.write(order_df.shape)
+        
+        else:
+            bms_df = pd.read_excel(uploaded_file)
+            # st.write(uploaded_file.name)
+            # st.write(bms_df.shape)
+
+    return order_df, bms_df
+
+
+def fill_data(bms_dataframe, order_dataframe):
+    """_summary_
+
+    Args:
+        bms_dataframe (_type_): _description_
+        order_dataframe (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    # Create a set to store duplicate 'Order No.' values
+    duplicate_order_numbers = set()
+    encountered_order_numbers = set()
+    c = 0
+    # Iterate through rows in bms_report_df
+    for idx, row in bms_dataframe.iterrows():
+        order_no = row['Order No.']
+        order_desc = row["Material Description"]
+        # Check if 'Order No.' is not NaN
+        if not pd.isna(order_no) and not pd.isna(order_desc):
+            # Check if the order_no has been encountered before
+            if order_no in encountered_order_numbers:
+                # Add it to the duplicate_order_numbers set
+                duplicate_order_numbers.add(order_no)
+            else:
+                # Otherwise, add it to the encountered_order_numbers set
+                encountered_order_numbers.add(order_no)
+            # Search for the corresponding row in order_df where 'PO No.' matches 'Order No.'
+            matching_rows = order_dataframe[order_dataframe['PO No.'] == order_no]
+            
+            if not matching_rows.empty:
+                # Extract data from the first matching row in order_df
+                for index, matching_row in matching_rows.iterrows():
+                    matching_desc = matching_row['Product Description']
+                    similarity = 1 - Levenshtein.distance(order_desc, matching_desc) / max(len(order_desc), len(matching_desc))
+                    if similarity > 0.60:
+                        size = matching_row['Size']
+                        unit_of_measure = matching_row['Unit of Measure']
+                        quantity = matching_row['Quantity']
+                        cas = matching_row['CAS #']
+                        
+
+                        bms_dataframe.at[idx, 'Size'] = size
+                        bms_dataframe.at[idx, 'Unit of Measure'] = unit_of_measure
+                        bms_dataframe.at[idx, 'Quantity'] = quantity
+                        bms_dataframe.at[idx, 'CAS #'] = cas
+    
+    return bms_dataframe, duplicate_order_numbers
+
+
+def convert_duplicate_order_to_dataframe(duplicate_order_numbers):
+    """_summary_
+
+    Args:
+        duplicate_order_numbers (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    duplicate_order_numbers_list = list(duplicate_order_numbers)
+    # Create a dataframe with the list of duplicate 'Order No.' values
+    duplicate_order_df = pd.DataFrame({'Duplicate Order No.': list(duplicate_order_numbers)})
+
+    return duplicate_order_df
+
+
+def remove_shipping(bms_dataframe):
+    """_summary_
+
+    Args:
+        bms_dataframe (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    processed_rows = set()
+    rows_to_remove = []
+    extra_charges = ["Shipping", "shipping", "Shipping/Handling Charges", "Carriage"]
+    
+    for index, row in bms_dataframe.iterrows():
+        for charge in extra_charges:
+            if pd.notna(row["Material Description"]) and charge in row["Material Description"]:
+                if index not in processed_rows:
+                    price = row["Inv Value  Currency-Original"]
+                    description = row["Material Description"]
+                    print(f" Index {index} Description {description} and Price {price}")
+                    value = bms_dataframe.at[index - 1, "Inv Value  Currency-Original"]
+                    if value < 0:
+                        bms_dataframe.at[index - 2, 'Other Costs'] = float(price)
+                        bms_dataframe.at[index - 2, 'Specify Other Cost'] = description
+                    else:
+                        bms_dataframe.at[index - 1, 'Other Costs'] = float(price)
+                        bms_dataframe.at[index - 1, 'Specify Other Cost'] = description
+                    rows_to_remove.append(index)
+                    processed_rows.add(index)  # Mark the row as processed
+
+    
+    return bms_dataframe, rows_to_remove
